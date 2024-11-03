ae522bdd82abcb069a52df3f917da43b04a2146a
elfua <elfua>
2024-11-03 19:49:21 +0100

    chore: update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const CopyPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
-
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
-// https://github.com/microsoft/monaco-editor/blob/c321d0fbecb50ab8a5365fa1965476b0ae63fc87/samples/browser-esm-webpack-typescript-react/webpack.config.js
-// module.exports = {
-
-//   // rest of the configuration
-// };
 
 module.exports = {
 
@@ -17,30 +11,22 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        // https://webpack.js.org/configuration/output/#outputchunkformat
-        // possible values: false string: 'array-push' | 'commonjs' | 'module' | <any string>
-        chunkFormat: 'array-push' // https://github.com/webpack/webpack/issues/3637#issuecomment-1911310809
+        chunkFormat: 'array-push',
+        clean: true,
     },
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.js$/,
-    //             exclude: /node_modules/,
-    //             use: 'babel-loader'
-    //         },
-    //         {
-    //             test: /\.css$/,
-    //             use: ['style-loader', 'css-loader']
-    //         }
-    //     ]
-    // },
+    // https://webpack.js.org/configuration/module/#modulegenerator
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|html|ico)$/i,
+                type: 'asset/resource',
+            }
         ],
+
     },
     plugins: [
         new MonacoWebpackPlugin({
@@ -54,17 +40,15 @@ module.exports = {
                 'xml',
                 'html',
                 'java',
-                'json'], // specify only needed languages
-        })
+                'json'],
+        }),
+        new CopyPlugin({
+            patterns: [
+              { from: "public"},
+            ],
+          }),
     ],
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: './public/index.html'
-    //     })
-    // ],
     devServer: {
-
-
         static: path.join(__dirname, 'dist'),
         liveReload: true,
         compress: true,
@@ -74,31 +58,22 @@ module.exports = {
         watchFiles: ['src/**/*', 'public/**/*']
     },
 
-    mode: process.env.NODE_ENV, // Enables minification
+    mode: process.env.NODE_ENV, 
     optimization: {
         minimize: true,
         minimizer: [
             new TerserPlugin({
-                // MUST SEE: 
-                //      https://terser.org/docs/options/
-                //      https://terser.org/docs/options/#format-options
                 extractComments: false,
                 parallel: true,
                 terserOptions: {
                     ecma: 2020,
-                    compress: 
-                     {
-                        // defaults: true,
+                    compress:
+                    {
                         drop_console: true,
                         drop_debugger: true,
-                        
-                    },
-                    // comments: false,
-                    // {
-                    //     // drop_console: true, // Remove console logs
-                    // },
-                    mangle: true,
 
+                    },
+                    mangle: true,
                 },
             }),
         ],
